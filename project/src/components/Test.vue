4bb02d366d18ba0d5acade2847eaace53e1ebe7b
Mandy-PC\Mandy <mtwmt@hotmail.com>
2018-12-17 22:28:24 +0800

    no message

---
+++ project/src/components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'BB',
+  data() {
+    return {
+      title: 'hello yooooooooo',
+    };
+  },
+};
+</script>
+
+<style scoped>
+  h1{
+    color: #159;
+  }
+</style>
