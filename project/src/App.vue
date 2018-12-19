3551fce21e9bde02cf0e0be44f6d4200fabf6c3b
Mandy-PC\Mandy <mtwmt@hotmail.com>
2018-12-20 01:28:14 +0800

    no message

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,24 +1,40 @@
 <template>
   <div id="app">
-    <h1>YouBike {{ city }}公共自行車即時資訊</h1>
+    <h1>YouBike {{ $route.params.city }}公共自行車即時資訊</h1>
     <nav>
-      <router-link to="/">台北市</router-link> |
-      <router-link to="/xinbei">新北市</router-link>
+      <router-link v-for="( link, idx ) in links" :key="idx" :to="'/'+link.name">{{ link.city + ' | ' }}</router-link>
+      <!-- <router-link to="/taipei">台北市</router-link> | 
+      <router-link to="/xinbei">新北市</router-link> -->
     </nav>
+    <br />
     <router-view />
   </div>
 </template>
 <script>
 
-
 export default {
-  name: 'app', 
+  name: 'app',
   data(){
     return{
-      city: '台北市',
-      data: [],
-      area: []
+      links: [
+        {city: '台北市', name: 'taipei'},
+        {city: '新北市', name: 'xinbei'}
+      ]
     }
+  },
+  computed: {
+    getLabel: function(){
+      let self = this;
+      return this.links.find(function( e ){
+        if( self.$route.name === e.name ) {
+          return self.$route.params.city = e.city
+        }
+      });
+    }
+  },
+  created(){
+    this.getLabel;
+    console.log( this.$route.name )
   }
 }
 </script>
--- project/src/router.js
+++ project/src/router.js
@@ -8,18 +8,14 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/taipei',
       name: 'taipei',
       component: taipei
     },
     {
       path: '/xinbei',
-      name: 'Xinbei',
+      name: 'xinbei',
       component: xinbei
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      // component: () => import(/* webpackChunkName: "about" */ './views/Xinbei.vue')
     }
   ]
 })
